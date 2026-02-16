ad2b8938f3a10892f068c99f3f17014b38b6648e
Ailton717 <ailtonlievano@gmail.com>
2026-02-16 00:54:37 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ailton's page</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <!-- Favicon -->
+  <link rel="icon" href="/REPO_AILTO/favicon.ico" type="image/x-icon">
+  <!-- Alternativa (si está en la misma carpeta que index.php):
+  <link rel="icon" href="favicon.ico" type="image/x-icon">
+  -->
+  <title>Aliton's page</title>
 </head>
 <body>
-    <h1>
-        "Bienvenido a la pagina de nuevo REY"
-    </h1>
+  <h1>"Bienvenido a la pagina de nuevo REY"</h1>
 </body>
 </html>
